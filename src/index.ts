1222a7576026be6a09d7add6ae6bcad8bf3ae95f
Dustin Yelland <dyelland@gmail.com>
2023-04-27 12:39:25 -0500

    api starts

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,19 @@
 import * as dotenv from 'dotenv';
 
-import { StateMachineWebserver } from 'mineflayer-statemachine';
 import * as db from './db/connect';
 import { loadBots } from './loaders';
+import bootstrap from './api/main';
 
 (async () => {
   dotenv.config();
 
-  await db.connect();
+  // await db.connect();
 
-  const bots = await loadBots({
-    host: process.env.HOST || 'localhost',
-    port: parseInt(process.env.PORT || '') || 25565,
-    version: process.env.version || '1.19.3',
-  });
+  await bootstrap();
 
-  bots.forEach((bot) => console.log(bot.bot.username));
-
-  // load webservers
-  // workers.forEach((worker, i) => {
-  //   const port = 3000 + i;
-
-  //   const webserver = new StateMachineWebserver(
-  //     worker.bot,
-  //     worker.stateMachine,
-  //     port,
-  //   );
-
-  //   webserver.startServer();
-
-  //   console.log(
-  //     `State machine viewer started for ${worker.bot.username} on http://localhost:${port}`,
-  //   );
+  // const bots = await loadBots({
+  //   host: process.env.HOST || 'localhost',
+  //   port: parseInt(process.env.PORT || '') || 25565,
+  //   version: process.env.version || '1.19.3',
   // });
 })();
